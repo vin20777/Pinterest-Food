0e1daed1089d737795f2edb25551b73bbf38f0cf
VTStudio <vin20777@gmail.com>
2017-12-16 15:25:21 +0800

    Initial Commit

---
+++ TWFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TWFood
+//
+//  Created by Vincent Tsao on 2017/12/16.
+//  Copyright © 2017年 VTStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
